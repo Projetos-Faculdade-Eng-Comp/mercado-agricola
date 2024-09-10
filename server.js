c97ad4cad1f46efe3fefe5e1c9cc072aa109ee2e
Víctor Chagas <victorchagaslm@gmail.com>
2024-09-10 16:50:57 -0300

    Removendo comentários

--- server.js
+++ server.js
@@ -7,24 +7,20 @@ const session = require("express-session");
 const app = express();
 const PORT = 3000;
 
-// Configuração do EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Configuração do Body-Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Configuração do express-session
 app.use(
   session({
-    secret: "mysecret", // Altere para uma chave secreta em produção
+    secret: "mysecret",
     resave: false,
     saveUninitialized: true,
   })
 );
 
-// Rota para exibir o formulário de cadastro
 app.get("/signUp", (req, res) => {
   const successMsg = req.session.successMsg || null;
   const errorMsg = req.session.errorMsg || null;
@@ -33,7 +29,6 @@ app.get("/signUp", (req, res) => {
   res.render("signUp", { successMsg, errorMsg });
 });
 
-// Rota para processar o formulário de cadastro
 app.post("/signUp", (req, res) => {
   const { primeiroNome, ultimoNome, email, senha, confirmSenha, perfil } =
     req.body;
@@ -43,15 +38,12 @@ app.post("/signUp", (req, res) => {
     return res.redirect("/signUp");
   }
 
-  // Lê o arquivo data/users.json
   fs.readFile("data/users.json", "utf8", (err, data) => {
     if (err) {
       if (err.code === "ENOENT") {
-        // Se o arquivo não existir, cria um novo array
         let users = [];
         users.push({ primeiroNome, ultimoNome, email, senha, perfil });
 
-        // Salva o arquivo data/users.json
         fs.writeFile(
           "data/users.json",
           JSON.stringify(users, null, 2),
@@ -67,10 +59,8 @@ app.post("/signUp", (req, res) => {
     } else {
       let users = JSON.parse(data);
 
-      // Adiciona novo usuário
       users.push({ primeiroNome, ultimoNome, email, senha, perfil });
 
-      // Salva o arquivo data/users.json
       fs.writeFile("data/users.json", JSON.stringify(users, null, 2), (err) => {
         if (err) throw err;
         req.session.successMsg = "Cadastro realizado com sucesso!";
@@ -80,18 +70,15 @@ app.post("/signUp", (req, res) => {
   });
 });
 
-// Rota para exibir a página de login
 app.get("/login", (req, res) => {
   const errorMsg = req.session.errorMsg || null;
   req.session.errorMsg = null;
   res.render("login", { errorMsg });
 });
 
-// Rota para processar o login
 app.post("/login", (req, res) => {
   const { email, senha } = req.body;
 
-  // Lê o arquivo data/users.json
   fs.readFile("data/users.json", "utf8", (err, data) => {
     if (err) throw err;
 
@@ -110,31 +97,26 @@ app.post("/login", (req, res) => {
   });
 });
 
-// Rota para a página inicial (index)
 app.get("/", (req, res) => {
   const user = req.session.user || null;
   res.render("index", { user });
 });
 
-// Rota para a página de produtores
 app.get("/producers", (req, res) => {
   const user = req.session.user || null;
   res.render("producers", { user });
 });
 
-// Rota para a página de produtos
 app.get("/products", (req, res) => {
   const user = req.session.user || null;
   res.render("products", { user });
 });
 
-// Rota para logout
 app.get("/logout", (req, res) => {
-  req.session.destroy(); // Destruir a sessão
+  req.session.destroy();
   res.redirect("/login");
 });
 
-// Inicializa o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
